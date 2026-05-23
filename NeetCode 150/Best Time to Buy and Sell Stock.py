c6e2c770700d3312eae34847d5be59de07c02adf
Ilham <142871024+IlhamIslahuddin@users.noreply.github.com>
2026-05-23 15:18:42 +0100

    Update Best Time to Buy and Sell Stock.py

--- NeetCode 150/Best Time to Buy and Sell Stock.py
+++ NeetCode 150/Best Time to Buy and Sell Stock.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profits = [-1 for i in range(len(prices))]
+        profits = [0 for i in range(len(prices))]
         for i in range(len(prices)):
             for j in range(i,len(prices)):
                 if prices[j] - prices[i] > profits[i]:
